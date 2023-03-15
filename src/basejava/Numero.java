3a6eb1524286735337c7893ac7a19b776c4a6b26
Iuri Alan <iurilan1996@gmail.com>
2023-03-15 18:32:11 -0300

    continua

--- src/basejava/Numero.java
+++ src/basejava/Numero.java
@@ -16,21 +16,16 @@ public class Numero {
 
     }
 
+// 1)Escreva um programa para ler 2 valores (considere que não serão informados valores iguais) e escrever o maior deles. 
+
     public int maior(int v1, int v2){
         if(v1 > v2){
             return v1;
         }
             return v2;
     }
+//Escreva um programa para ler 3 valores inteiros (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente. 
+
     public String ordemcresc(int n1, int n2, int n3){
-        if(n1>n2>n3){
-            return n1 - n2 - n3
-        } if(n1<n2<n3){
-            return n3 - n2 - n1
-        } if(n2>n1>n3){
-            return n2 - n1 - n3
-        } if(n2<n1<n3){
-            return n3 - n1 - n2
-        }
-    }
+
 }
