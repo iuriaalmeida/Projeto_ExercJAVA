679877dca2af0fc4cfa2b4bbef92c03b74acedca
Iuri Alan <iurilan1996@gmail.com>
2023-03-20 21:40:52 -0300

    aula5

--- src/basejava/retangulo/Principal.java
+++ src/basejava/retangulo/Principal.java
@@ -1,17 +1,33 @@
 package basejava.retangulo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
+
+import basejava.quadrado.Quadrado;
 
 public class Principal {
     public static void main(String[] args){
-        Retangulo retangulo = new Retangulo();
-        retangulo.mudarBase(10);
-        retangulo.mudarAltura(5);
-
-        JOptionPane.showMessageDialog(null, ""
-        + "Valor da base: " + retangulo.mostrarBase() + "\n" 
-        + "Valor da altura: " + retangulo.mostrarAltura() + "\n"
-        + "Valor da Área: " + retangulo.calcularArea() + "\n"
-        + "Valor do Perímetro: " + retangulo.calcularPeri() + "\n");
+        List<Retangulo> retangulos = new ArrayList<>();
+        String dadosEntrada = " ";
+        while (!dadosEntrada.equals("")){
+            try {
+                
+                dadosEntrada = JOptionPane.showInputDialog(null, "Qual o tamanho da base do retangulo?");
+                if (!dadosEntrada.equals("")){
+                    Retangulo retangulo = new Retangulo();
+                    retangulo.setBase(Float.parseFloat(dadosEntrada));
+                dadosEntrada = JOptionPane.showInputDialog(null, "Qual o tamanho da altura do retangulo?");
+                    retangulo.setAltura(Float.parseFloat(dadosEntrada));
+                    JOptionPane.showMessageDialog(null, ""
+                        + "Tamanho da base: " + retangulo.getBase() + "\n"
+                        + "Area do Quadrado: " + quadrado.calcularArea() + "\n");
+                    retangulo.add(retangulo);
+                }                    
+            } catch (Exception e){
+                JOptionPane
+            }
+        }
     }
 }
